a0a744cd4db1a0ebb1f9feb6d01918b15d6ed38f
6314089 <6314089@ed.tus.ac.jp>
2015-03-08 08:23:44 +0900

    fix script.js

--- src/js/script.js
+++ src/js/script.js
@@ -179,7 +179,7 @@ var Page = function(container){
           }
           for(i = 1; i <= gallery.length; i++){
             document.getElementById('item'+i).addEventListener('click',
-              this.display.bind(i)
+              console.log(this)
             );
           }
       });
